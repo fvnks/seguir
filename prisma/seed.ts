bda36974a27772993d8885064aa01b6a6f63394f
fvnks <fvnks@users.noreply.github.com>
2025-11-02 19:13:29 -0300

    fix: Update import path for PrismaClient in seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Role } from '../src/generated/prisma/client';
+import { PrismaClient, Role } from '@prisma/client';
 import * as bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
